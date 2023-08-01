d99d1ae2546a4e5b737a2bcff8217790b02fbc1e
AarjavJain101 <Aarjavjain2736@gmail.com>
2023-08-01 17:47:41 -0600

    Edited/Completed Beat Detection section

--- src/components/BeatDetector.js
+++ src/components/BeatDetector.js
@@ -1,53 +1,90 @@
 import React from "react";
 import "../styles/BeatDetector.css";
-import BeatDetectorImage from "../images/Beat_detector.png";
+import CBeatDetectorGif from "../images/C_Version_I-Won.gif";
+import PythonBeatDetectorGif from "../images/Python_Version_I-Won.gif";
 
 const BeatDetector = () => {
     return (
         <div className="beat-detector-container">
             <div className="beat-detector-title">
-                <h1 className="beat-detector-title-text">Beat Detector for LED Lights</h1>
+                <a target="_blank" href="https://beat-detector.netlify.app/" className="beat-detector-link">
+                    <h1 className="beat-detector-title-text">Beat Detector for LED Lights</h1>
+                </a>
             </div>
             <div className="beat-detector-content">
                 <div className="beat-detector-text">
                     <p className="beat-detector-text-paragraph">
                         Beat detection algorithm to detect bass, claps, and hi-hats in hip-hop/rap music.
+                        Python speciality is <strong>synched lyrics</strong>. C\C++ speciality is{" "}
+                        <strong>fade effect and creative potential.</strong>
                         <br />
                         <br />
                         The <strong>Python</strong> implementation utilizes <strong>PyAudio</strong>,{" "}
-                        <strong>OpenCV</strong>, and <strong>Matplotlib</strong>, and <strong>Numpy</strong>{" "}
-                        to perform and display the detection. Additionally, the <strong>TKinter</strong>{" "}
-                        module is used to create the Lightroom experience. Note that the program which creates
-                        videos utilizes <strong>FFmpeg</strong>.
+                        <strong>OpenCV</strong>, <strong>Matplotlib</strong>, and <strong>Numpy</strong> to
+                        compute the detection. Additionally, the <strong>TKinter</strong> module,{" "}
+                        <strong>Spotify API</strong>, and <strong>synched lyrics API</strong> was used to
+                        create the Lyrics-Light-Room experience. Note that the data visualization program
+                        utilizes <strong>FFmpeg</strong>.
                         <br />
                         <br />
                         The <strong>C/C++</strong> implementation utilizes libraries such as{" "}
-                        <strong>portaudio.h</strong> and <strong>fftw3.h</strong> among other standard
-                        libraries.
+                        <strong>portaudio.h</strong>, <strong>fftw3.h</strong>, <strong>windows.h</strong>,
+                        and other standard libraries to create a GUI for the light room experience. Credit to{" "}
+                        <a
+                            target="_blank"
+                            href="https://www.linkedin.com/in/rhys-byers-b54162261/"
+                            className="rhys-credit"
+                        >
+                            Rhys Byers
+                        </a>{" "}
+                        for developing the GUI.
                         <br />
                         <br />
                         The Web Browser implementation was created using <strong>React.js</strong> and{" "}
-                        <strong>Web Audio API</strong>, however, does not perform as well as the C++ or Python
-                        versions due to parameter constraints by Web Audio API.
+                        <strong>Web Audio API</strong>, however, does not perform nearly as well as the C++ or
+                        Python versions due to parameter constraints by Web Audio API.
                         <br />
                         <br />
-                        Please click on the image to see the web version. From there access the github.
+                        <strong>
+                            Please click on the GIFS to see the github. Click the title to see the website.
+                        </strong>
                     </p>
                 </div>
                 <div className="beat-detector-image-container">
                     <a
                         href="
-                        https://beat-detector.netlify.app/"
+                        https://github.com/AarjavJain101/Beat-Detector"
                         target="_blank"
+                        className="beat-detector-image-links"
                     >
-                        <img
-                            src={BeatDetectorImage}
-                            alt="Beat Detector Terminal"
-                            className="beat-detector-image"
-                            draggable="false"
-                        />
+                        <div className="beat-detector-images">
+                            <div className="image-container">
+                                <img
+                                    src={PythonBeatDetectorGif}
+                                    alt="Python Beat Detector Terminal"
+                                    className="python-gif"
+                                    draggable="false"
+                                />
+                                <div className="image-caption-beat">
+                                    <span className="description">
+                                        Python Beat Detector with Synced Lyrics
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="image-container">
+                                <img
+                                    src={CBeatDetectorGif}
+                                    alt="C Beat Detector Terminal"
+                                    className="c-gif"
+                                    draggable="false"
+                                />
+                                <div className="image-caption-beat">
+                                    <span className="description">C/C++ Beat Detector with Fade Effect</span>
+                                </div>
+                            </div>
+                        </div>
+                        <h4 className="beat-detector-caption">(Click on GIFs)</h4>
                     </a>
-                    <h4 className="image-caption-beat">(Click on image)</h4>
                 </div>
             </div>
         </div>
