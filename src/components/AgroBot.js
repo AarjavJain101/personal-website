aef81bcf3ed81bb004c26903e402e34747a94ce8
AarjavJain101 <Aarjavjain2736@gmail.com>
2023-08-02 23:06:41 -0600

    Updated tense

--- src/components/AgroBot.js
+++ src/components/AgroBot.js
@@ -10,9 +10,9 @@ const AgroBot = () => {
             <h1 className="agrobot-title">UBC AgroBot: Applied AI Developer</h1>
             <img src={AgroBotLogo} alt="Agro bot logo" className="agrobot-logo" draggable="false" />
             <p className="agrobot-contouring-text">
-                As a first year developer at UBC AgroBot I work mainly in Python create practical algorithms
-                for image recognition. I am learning OpenCV and machine learning to fulfill my role in
-                developing a leaf area estimation model for our robot. My current task is developing a program
+                As a first year developer at UBC AgroBot I worked mainly in Python to create practical
+                algorithms for image recognition. I learned OpenCV and machine learning to fulfill my role in
+                developing a leaf area estimation model for our robot. Specifically, I developed a program
                 that identifies contours of plants while disregarding weeds.
             </p>
             <img
@@ -22,9 +22,9 @@ const AgroBot = () => {
                 draggable="false"
             />
             <p className="crop-detection">
-                More recently, I have been working on training a YOLOv8 to perform object detection on plant
-                images provided in the 2nd ACRE Cascade Competition. The image below is an example of a
-                prediction generated during inference:
+                Additionally, I have worked on training a YOLOv8 to perform object detection on plant images
+                provided in the 2nd ACRE Cascade Competition. The image below is an example of a prediction
+                generated during inference:
             </p>
             <img
                 src={BoundingBoxImage}
