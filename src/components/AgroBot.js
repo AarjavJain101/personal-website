584f5b1063dc29bc6b07894101b8a98940b106ad
AarjavJain101 <Aarjavjain2736@gmail.com>
2023-09-29 11:53:14 -0700

    Added Solar

--- src/components/AgroBot.js
+++ src/components/AgroBot.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/AgroBot.css";
+import SolarLogo from "../images/solar.png";
 import AgroBotLogo from "../images/website.png";
 import ContouringImage from "../images/contouring_plants_website.png";
 import BoundingBoxImage from "../images/bounding_boxes.png";
@@ -7,7 +8,17 @@ import BoundingBoxImage from "../images/bounding_boxes.png";
 const AgroBot = () => {
     return (
         <div className="agrobot">
+            <div className="solar">
+                <h1 className="agrobot-title">UBC Solar: Embedded Systems Engineer</h1>
+                <img src={SolarLogo} alt="Solar logo" className="agrobot-logo" draggable="false" />
+                <p className="agrobot-contouring-text">
+                    Currently, I am an embedded systems engineer at UBC Solar. My work centers around
+                    developing the firmware for our MCB, AMB, DID, and TEL boards. Additionally, I am becoming
+                    proficint in serial communication, C\C++, STM32 chips and STM32Cube IDE.
+                </p>
+            </div>
             <h1 className="agrobot-title">UBC AgroBot: Applied AI Developer</h1>
+            <h1 className="agrobot-title-2">January - May 2023</h1>
             <img src={AgroBotLogo} alt="Agro bot logo" className="agrobot-logo" draggable="false" />
             <p className="agrobot-contouring-text">
                 As a first year developer at UBC AgroBot I worked mainly in Python to create practical
